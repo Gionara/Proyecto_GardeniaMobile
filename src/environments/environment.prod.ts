71eeec0cde9df782044949f7a846f89ada8eba2c
Giovanna Narayana Espinosa Jara <gi.espinosa@duocuc.cl>
2024-11-20 18:46:04 -0300

    apk config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCqqy03xjS8XmkH50tNrLPuqDwNnhMc8jo",
+    authDomain: "gardeniaapp-366ee.firebaseapp.com",
+    projectId: "gardeniaapp-366ee",
+    storageBucket: "gardeniaapp-366ee.appspot.com",
+    messagingSenderId: "71915271929",
+    appId: "1:71915271929:web:498b2f844882ca214ed428",
+    measurementId: "G-XDCE8WR8BE",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,22 @@
-export const environment = {
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
+export const environment: { production: boolean; firebaseConfig: FirebaseConfig } = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyCqqy03xjS8XmkH50tNrLPuqDwNnhMc8jo",
     authDomain: "gardeniaapp-366ee.firebaseapp.com",
     projectId: "gardeniaapp-366ee",
-    storageBucket: "gardeniaapp-366ee.firebasestorage.app",
+    storageBucket: "gardeniaapp-366ee.appspot.com",
     messagingSenderId: "71915271929",
     appId: "1:71915271929:web:498b2f844882ca214ed428",
-    measurementId: "G-XDCE8WR8BE"
-  }
+    measurementId: "G-XDCE8WR8BE",
+  },
 };
